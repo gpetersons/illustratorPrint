338c094f45f9ccd3d54ecf10a56a31646b6a28e9
Guntars <guntars.petersons@gmail.com>
2016-02-03 17:11:20 +0200

    Update print.jsx
    Save last print count to ini

--- print.jsx
+++ print.jsx
@@ -7,6 +7,28 @@ var localPrintOptions = new PrintOptions();
 localPrintOptions.printPreset = localPrintPreset;
 var myJobOptions = new PrintJobOptions();
 
+//Get fullPath to Script file
+var thisFile = new File($.fileName);
+var pathJSX = thisFile.path;
+
+//Read  INI File
+var myINIFile = new File(pathJSX+'/LastPrintCount.ini');
+var openIni = myINIFile.open('e'); // 'e' read-write open file.
+if (myINIFile.length == 0) {
+	//Write  default INI file
+	myINIFile. writeln('1');
+	myINIFile.close();
+	myINIFile = new File(pathJSX+'/LastPrintCount.ini');
+	var openIni = myINIFile.open('e'); // 'e' read-write open file.
+}
+
+if (!openIni){
+	var LastPrintCount = '1';
+} else {
+	var LastPrintCount = myINIFile. readln();
+	myINIFile.close();
+}
+
 var res =
 	"dialog {properties: {borderless: false, closeButton: true}, text: 'Print:',\
 		txtDaudzums: EditText {text: '1', justify: 'center', preferredSize: [170,30]}, \
@@ -18,9 +40,11 @@ var res =
 }";
 
 win = new Window(res);
+win.txtDaudzums.text = LastPrintCount;
 win.txtDaudzums.active = true;
 
 win.buttons.okBtn.onClick = function(){
+	writeINI(win.txtDaudzums.text)
 	doPrint();
 	win.close();
 }
@@ -44,4 +68,11 @@ function doPrint(){
 			tmpDaudz = tmpDaudz - 999; 
 		}
 	}
+}
+
+function writeINI(LastPrintCount){
+	myINIFile = new File(pathJSX+'/LastPrintCount.ini');
+	myINIFile.open('e'); // 'e' read-write open file.
+	myINIFile. writeln(LastPrintCount);
+	myINIFile.close();
 }
